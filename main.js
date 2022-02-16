05fd92fc7eb26e558ad8d2ef19fd6e382111e290
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-16 01:49:41 +0100

    fine css

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // console.log('prova')
 
+// L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata 
+// Ogni cella contiene un numero tra quelli compresi in un range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+
 // ------------------------------------
